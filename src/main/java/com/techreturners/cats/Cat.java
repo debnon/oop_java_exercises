1020dc2f0b7eee831df76181740fb75d6f409e1f
Ben Donaghy <bendonaghy321@gmail.com>
2022-01-12 10:00:32 +0000

    added cat classes

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -1,5 +1,12 @@
 package com.techreturners.cats;
 
 interface Cat {
+    boolean sleep = false;
 
+    public void isAsleep();
+    public void goToSleep();
+    public void wakeUp();
+    public void getSetting();
+    public void getAverageHeight();
+    public void eat();
 }
